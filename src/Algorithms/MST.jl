aad767125a1b2dd0619baf0c23fc9a7740c32a3c
gavingleinig <gavingleinig@gmail.com>
2026-04-06 22:05:29 -0500

    Add MST, pt 1 of MFC steps

--- src/Algorithms/MST.jl
+++ src/Algorithms/MST.jl
@@ -1,1 +1,57 @@
-# Perhaps use Julia's Graph.jl?
+using DataStructures
+
+struct WeightedEdge{T<:Real} # T must be a subtype of Real, ie Int, Float64, etc
+    weight::T
+    a::Int
+    b::Int
+end
+
+# Tell Julia how to compare WeigthedEdges
+Base.isless(e1::WeightedEdge, e2::WeightedEdge) = isless(e1.weight, e2.weight)
+
+# Compute Optimum MST for given graph
+function mst!(n::Int, edges::Vector{WeightedEdge{T}}) where T
+    sort!(edges) # uses above comparison, Base.isless
+    dj_sets = IntDisjointSets(n)
+    kept_edges = WeightedEdge{T}[]
+    
+    for e in edges
+        if !in_same_set(dj_sets, e.a, e.b) # If edge connects two verticies in different trees:
+            union!(dj_sets, e.a, e.b) # merge two trees
+            push!(kept_edges, e) # add edge to MST
+            
+            # Step when there are n-1 edges in MST
+            length(kept_edges) == n - 1 && break
+        end 
+    end
+    return kept_edges
+end
+
+# Takes list of points and distance function, returns list of edges with distances pre-calculated
+function mst_implicit(points::AbstractVector{V}, dist_func::F) where {V, F}
+    n = length(points)
+    T = Base.promote_op(dist_func, eltype(points), eltype(points)) # Infer return type of distance function
+
+    if n < 2
+        return WeightedEdge{T}[] 
+    end
+    
+    edges = WeightedEdge{T}[]
+    
+    # Batched implicit generation
+    for i in 1:(n-1)
+        for j in (i+1):n
+            w = dist_func(points[i], points[j])
+            push!(edges, WeightedEdge(w, i, j))
+        end
+        
+        
+        if length(edges) > 10_000_000 # Batch threshold 
+            # Compute MST and prune
+            edges = mst!(n, edges)
+        end
+    end
+    
+    # Final MST calculation
+    return mst!(n, edges)
+end
--- src/Algorithms/MetricForests.jl
+++ src/Algorithms/MetricForests.jl
@@ -1,21 +1,71 @@
 # src/Algorithms/MetricForests.jl
 
-struct MFCResult{T<:AbstractFloat}
-    cluster_edges::Vector{Tuple{Int, Int}}      
-    completion_edges::Vector{Tuple{Int, Int}}   
-    sub_cluster_runtime::T
-    completion_edges_runtime::T
-    completion_runtime::T
+struct MFCResult
+    cluster_edges::Vector{Tuple{Int, Int}}      # tuples representing the edges that form the MST within each local cluster
+    completion_edges::Vector{Tuple{Int, Int}}   # tuples representing the edges that bridge the separate clusters
+    sub_cluster_runtime::Float64                # time (in seconds) taken to compute the local cluster MSTs
+    completion_edges_runtime::Float64           # time (in seconds) taken to find the connecting edges
+    completion_runtime::Float64                 # total runtime of the MFC algorithm
 end
 
 function metric_forest_completion(
-    points::AbstractVector{V}, 
-    cluster_count::Int, 
-    cluster_assignments::Vector{Int}, 
+    points::AbstractVector{V},          # An array of type V representing data points
+    cluster_count::Int,                 # The total number of clusters the data was divided into initial clustering
+    cluster_assignments::Vector{Int},   # An array mapping each point to its assigned cluster ID. The length must exactly match the length of 'points.' Values should range from '1' to 'cluster_count'.
     dist_func::F
-) where {V<:AbstractVector, F<:Function}
+) where {V, F<:Function}
     
-    # TODO
+    total_start_time = time()
     
-    return MFCResult{Float64}(Tuple{Int, Int}[], Tuple{Int, Int}[], 0.0, 0.0, 0.0) 
+    cluster_edges = Tuple{Int, Int}[]
+    completion_edges = Tuple{Int, Int}[]
+    
+    ### MST Each Cluster ###
+    sub_cluster_start = time()
+    
+    # Group original point indices by their assigned cluster
+    cluster_indices = [Int[] for _ in 1:cluster_count] # init vectors holding clusters
+    for (global_index, cluster_id) in enumerate(cluster_assignments)
+        push!(cluster_indices[cluster_id], global_index)
+    end
+    
+    # Run MST on each cluster
+    for c in 1:cluster_count
+        local_indices = cluster_indices[c]
+        
+        # A cluster must have at least 2 points to form an edge
+        length(local_indices) < 2 && continue
+        
+        # Extract data for points in cluster
+        cluster_points = points[local_indices]
+        
+        local_mst_edges = mst_implicit(cluster_points, dist_func)
+        
+        # Map the local MST indices back to the global dataset indices
+        for edge in local_mst_edges
+            global_a = local_indices[edge.a]
+            global_b = local_indices[edge.b]
+            push!(cluster_edges, (global_a, global_b))
+        end
+    end
+    
+    sub_cluster_runtime = time() - sub_cluster_start
+
+    ### Completeion Edges ###
+    completion_edges_start = time()
+    
+    # TODO: Implement the completion phase.
+    # Ie, find shortest edges between the disconnected clusters to form global MST, or representatives, etc...
+    
+    completion_edges_runtime = time() - completion_edges_start
+    
+    total_runtime = time() - total_start_time
+
+    return MFCResult(
+        cluster_edges, 
+        completion_edges, 
+        sub_cluster_runtime, 
+        completion_edges_runtime, 
+        total_runtime
+    ) 
 end
