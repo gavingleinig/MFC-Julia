dc2d022d4f6606cc1e17ef41b0cb45e5b7bf0f21
balar0 <144611586+balar0@users.noreply.github.com>
2026-04-08 22:10:04 -0500

    strated clustering completion code

---
+++ src/Algorithms/MetricForestCompletionConnection.jl
@@ -0,0 +1,39 @@
+
+
+
+
+function metric_forest_completion_Edges_approx_simple(
+    points::AbstractVector{V},          # An array of type V representing data points
+    cluster_count::Int,                 # The total number of clusters the data was divided into initial clustering
+    cluster_assignments::Vector{Int},   # An array mapping each point to its assigned cluster ID. The length must exactly match the length of 'points.' Values should range from '1' to 'cluster_count'.
+
+    dist_func::F
+) where {V, F<:Function}
+
+
+    # Consider all clusters 
+    for i in 1:cluster_count
+        for j in 2:cluster_count
+
+
+
+
+            
+
+
+        end
+
+
+    end
+
+
+
+
+
+    # Choose
+
+
+
+
+
+end
