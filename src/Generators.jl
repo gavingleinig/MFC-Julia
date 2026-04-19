ad287a5be283533f2b16b4259c03a39fa18a8c3a
balar0 <144611586+balar0@users.noreply.github.com>
2026-04-19 15:33:50 -0500

    added generators

--- src/Generators.jl
+++ src/Generators.jl
@@ -1,5 +1,7 @@
 # Synthetic Data Generation
 # Replace gaussian_point_gen.cpp and unifrom.cpp
+using Random
+using Distributions
 
 # Type{T} argument allows users to call generate_gaussians(Float32, dim=...) to get 32-bit floats
 # Unsure if this is doing too much.
@@ -15,14 +17,27 @@ function generate_gaussians(
     total_points = num_centers * points_per_center
     points = Vector{Vector{T}}(undef, total_points)
     labels = Vector{Int}(undef, total_points)
+    # gerneate the random number
+    g = Xoshiro(123)
+
+
     
     idx = 1
     for cluster_id in 1:num_centers
         # TODO: Setup standard normal distributions for this cluster_id w/ mean_range and sigma_range
         
+        # First randimoly generate Mean for cluster
+        cluster_mean = (rand(g) * (mean_range[1] - mean_range[2] + 1)) + mean_range[1]
+
+        # Next randimly genearte sigma for cluster
+        cluster_sigma = (rand(g) * (sigma_range[1] - sigma_range[2] + 1)) + sigma_range[1]
+
+        # Finally create nomral distibution
+        d = Normal(cluster_mean, cluster_sigma)
+
         for p in 1:points_per_center
             # TODO: Generate individual point 
-            # points[idx] =
+            points[idx] = rand(d,dim)
             
             labels[idx] = cluster_id 
             idx += 1
